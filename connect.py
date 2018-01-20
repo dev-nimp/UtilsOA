c2335a33283dabbe2f7040c5ec015417d98155f0
niMp <dev.koyvistoynen@gmail.com>
2018-01-21 01:12:11 +0300

    Реализовано подключение к БД и первичная выборка

--- connect.py
+++ connect.py
@@ -1,7 +1,13 @@
 import pyodbc
 
-cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=DELOWEB-SERVER\SQLTEST;DATABASE=DBTEST;UID=sa;PWD=1Nz3Z88Lc')
+cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=DESKTOP-4C23489\SQLEXPRESS;DATABASE=DBTEST;UID=admin;PWD=421744Tz')
 
 cursor = cnxn.cursor()
-cursor.execute("INSERT INTO AbUser(user) values ('111')")
-cnxn.commit()
+#cursor.execute("INSERT INTO dbo.AbUser (user) values ('111')")
+cursor.execute("SELECT * FROM dbo.AbUser")
+row = cursor.fetchone()
+while row:
+    print(str(row[0]))
+    row = cursor.fetchone()
+
+#cnxn.commit()
